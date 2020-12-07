7ed74a3bd5be2cf317f2e1f7e472fd63f4fa0072
ghost <arkaghosh305@gmail.com>
2020-12-08 00:05:22 +0530

    general commit

--- event pop-up/ux_definitions.py
+++ event pop-up/ux_definitions.py
@@ -1,7 +1,13 @@
 from ui_definitions import *
+import pickle
 
 
 def b_ok(event=None):
+    global responses
+
+    with open('data.dat', 'wb') as f:
+        pickle.dump(str(responses), f)
+
     root.destroy()
 
 
