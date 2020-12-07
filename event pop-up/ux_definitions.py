be794c12ac5bf8e3ba4610b699c665829b56fede
ghost <arkaghosh305@gmail.com>
2020-12-07 11:57:23 +0530

    general commit

--- event pop-up/ux_definitions.py
+++ event pop-up/ux_definitions.py
@@ -2,11 +2,6 @@ from ui_definitions import *
 
 
 def b_ok(event=None):
-    global entries
-
-    for i in range(len(l)):
-        print(entries[i]['label'], entries[i]['response'].get(), sep=': ')
-
     root.destroy()
 
 
