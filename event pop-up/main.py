696903d319fcb6e0c35f339ad74bb8af13fd51c6
ghost <arkaghosh305@gmail.com>
2020-12-07 11:54:20 +0530

    general commit

--- event pop-up/main.py
+++ event pop-up/main.py
@@ -8,7 +8,6 @@ b_cancel = tkinter.Button(canvas, text='Cancel', command=ux_definitions.b_cancel
 b_ok.place(x=width - 120, y=height - 30)
 b_cancel.place(x=width - 70, y=height - 30)
 
-root.bind('<Return>', ux_definitions.b_ok)
 
 canvas.pack()
 root.mainloop()
--- event pop-up/ux_definitions.py
+++ event pop-up/ux_definitions.py
@@ -2,16 +2,9 @@ from ui_definitions import *
 
 
 def b_ok(event=None):
-    global e
-
-    print(e[0].get())
 
     root.destroy()
-
-    return True
 
 
 def b_cancel():
     root.destroy()
-
-    return False
