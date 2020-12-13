97ad8357ca71987e919155e3db5887b6595f1ce8
ghost1608 <64543976+ghost-1608@users.noreply.github.com>
2020-12-13 18:11:26 +0530

    Revert "general commit"
    This reverts commit 030ca5e22f00ece206b20cd53d11d1323a7d482d.

--- event pop-up/ux_definitions.py
+++ event pop-up/ux_definitions.py
@@ -5,7 +5,7 @@ import hashlib
 
 
 def b_ok(event=None):
-    global e, C, o, m, months
+    global e, C, o, m
 
     responses = []
 
@@ -24,10 +24,10 @@ def b_ok(event=None):
 
     x = pickle.dumps(str(responses))
 
-    with open('DATA.BIN', 'wb') as f:
+    with open('KEY.BIN', 'wb') as f:
         f.write(hmac.new(b'shared-key', x, hashlib.sha256).digest())
 
-    with open('Storage.dat', 'wb') as f:
+    with open('data.dat', 'wb') as f:
         f.write(x)
 
     root.destroy()
