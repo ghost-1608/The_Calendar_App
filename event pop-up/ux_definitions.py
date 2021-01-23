bf0f38ff139f34dc323d4ec9d5ddd5afa0f2a026
ghost <arkaghosh305@gmail.com>
2021-01-23 20:44:36 +0530

    general commit

--- event pop-up/ux_definitions.py
+++ event pop-up/ux_definitions.py
@@ -29,23 +29,53 @@ def b_ok(event=None):
 
     if os.path.exists('Storage.dat') and os.path.getsize('Storage.dat'):
         with open('Storage.dat', 'rb+') as f:
-            with open('DATA.BIN', 'rb') as g:
-                ke = g.read()
+            if os.path.exists('DATA.BIN'):
+                with open('DATA.BIN', 'rb') as g:
+                    ke = g.read()
 
-            i = f.read()
-            if hmac.new(b'shared-key', i, hashlib.sha256).digest() == ke:
-                di = eval(pickle.loads(i))
-                di[str(datetime.datetime.now().strftime('%d') + datetime.datetime.now().strftime('%m') + datetime.datetime.now().strftime('%Y'))] += [responses]
+                i = f.read()
+                if hmac.new(b'shared-key', i, hashlib.sha256).digest() == ke:
+                    di = eval(pickle.loads(i))
 
-                x = pickle.dumps(str(di).encode())
+                    if str(int(responses[2]) + 100)[1::] + str(months.index(responses[3]) + 101)[1::] + responses[4] not in di.keys():
+                        di[str(int(responses[2]) + 100)[1::] + str(months.index(responses[3]) + 101)[1::] + responses[4]] = []
+                    di[str(int(responses[2]) + 100)[1::] + str(months.index(responses[3]) + 101)[1::] + responses[4]] += [responses]
 
-                with open('DATA.BIN', 'wb') as g:
-                    g.write(hmac.new(b'shared-key', x, hashlib.sha256).digest())
+                    x = pickle.dumps(str(di).encode())
 
-                f.seek(0)
-                f.write(x)
+                    with open('DATA.BIN', 'wb') as g:
+                        g.write(hmac.new(b'shared-key', x, hashlib.sha256).digest())
+
+                    f.seek(0)
+                    f.write(x)
+                else:
+                    root.destroy()
+                    print('Error! Storage has been corrupted!')
+                    ch = input("Enter 'y' to delete all storage: ")
+                    if ch == 'y':
+                        try:
+                            f.close()
+                            g.close()
+                            os.remove('Storage.dat')
+                            os.remove('DATA.BIN')
+                            print('Deleted all storage!')
+                        except:
+                            print("Error! Couldn't delete storage!")
+
+                root.destroy()
+            else:
+                root.destroy()
+                print('Error! Bin file missing.')
+                ch = input("Enter 'y' to delete storage file: ")
+                if ch == 'y':
+                    try:
+                        f.close()
+                        os.remove('Storage.dat')
+                        print('Deleted storage!')
+                    except:
+                        print("Error! Couldn't delete storage!")
     else:
-        x = pickle.dumps(str({str(datetime.datetime.now().strftime('%d') + datetime.datetime.now().strftime('%m') + datetime.datetime.now().strftime('%Y')): [responses]}).encode())
+        x = pickle.dumps(str({str(int(responses[2]) + 100)[1::] + str(months.index(responses[3]) + 101)[1::] + responses[4]: [responses]}).encode())
 
         with open('Storage.dat', 'wb') as f:
             f.write(x)
@@ -53,7 +83,7 @@ def b_ok(event=None):
         with open('DATA.BIN', 'wb') as f:
             f.write(hmac.new(b'shared-key', x, hashlib.sha256).digest())
 
-    root.destroy()
+        root.destroy()
 
 
 def b_cancel():
