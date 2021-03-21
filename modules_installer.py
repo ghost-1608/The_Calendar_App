e767ab01b37c9901b6b813d1ce630400b8361844
ghost <arkaghosh305@gmail.com>
2021-03-22 00:46:43 +0530

    added modules_installer.py

---
+++ modules_installer.py
@@ -0,0 +1,12 @@
+import subprocess
+import sys
+
+try:
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-r', 'modules.txt'])
+except subprocess.CalledProcessError:
+    print('Error!\nThere was a problem trying to install the required modules.')
+    print('Make sure that modules_installer.py was run with administrator privileges.')
+    print('If the problem persists, please install the modules given in modules.txt manually.\n')
+
+input('Press enter to exit... ')
+sys.exit()
